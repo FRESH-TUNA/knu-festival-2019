a8e02e427749d3b63893524a803e359c18edc1fb
DONGWON KIM <lunacircle4@likelion.org>
2020-06-04 15:44:02 +0900

    feat: nginx deployment

---
+++ nginx/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# migrate
+docker-compose run --entrypoint="sh ./docker/prod/asset_compile.sh" web
+cp -r ../web/static ./
+rm -rf ../web/static
+
+# finish
+docker build -t lunacircle4/knufestival2019-nginx:latest .
